4cb9178bb238507c3ae5fc40cb2e8a1a886ffd7c
GorbachevaPolina <gorbacheva.polina7394@gmail.com>
2023-05-02 10:59:06 +0300

    initialised project

---
+++ src/components/sidebar/sidebar.tsx
@@ -0,0 +1,25 @@
+import React, {FC} from "react";
+
+const Sidebar: FC = () => {
+    return (
+        <div>
+            <img />
+            <nav>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Sidebar;
